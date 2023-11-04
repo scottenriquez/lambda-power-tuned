d626215cae6883fb03bfc968e270e0b7da759fb1
Scottie Enriquez <scottie@enriquez.xxx>
2023-11-03 18:17:24 -0700

    Adding README

--- lambda_power_tuned/lambda_power_tuned/terraform/lambda.tf
+++ lambda_power_tuned/lambda_power_tuned/terraform/lambda.tf
@@ -16,7 +16,7 @@ resource "random_id" "iam_role_name" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-	name               = "iam_for_lambda-${random_id.iam_role_name}"
+	name               = "iam_for_lambda-${random_id.iam_role_name.id}"
 	assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -26,10 +26,14 @@ data "archive_file" "lambda" {
 	output_path = "lambda_function_payload.zip"
 }
 
+resource "random_id" "lambda" {
+    byte_length = 8
+}
+
 resource "aws_lambda_function" "test_lambda" {
 	architectures = ["arm64"]
 	filename      = "lambda_function_payload.zip"
-	function_name = "target_function"
+	function_name = "target_function-${random_id.lambda.id}"
 	role          = aws_iam_role.iam_for_lambda.arn
 	handler       = "lambda_function.lambda_handler"
 	memory_size   = 128
